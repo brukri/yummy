ea6ef6339cbd3df4c2310f852e501013793d72ad
Bruno Krieg <bruno_krieg@gmx.ch>
2018-08-29 21:07:14 +0200

    Fix playground html template and change port

--- src/app/spoonacular.service.ts
+++ src/app/spoonacular.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const SERVICE_URI = 'http://localhost:3000';
+const SERVICE_URI = 'http://localhost:3100';
 
 @Injectable({
   providedIn: 'root'
