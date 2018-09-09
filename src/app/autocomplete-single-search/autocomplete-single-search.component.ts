e92d0b871909812d668ed6d868cd184d4e7822aa
Bruno Krieg <bruno_krieg@gmx.ch>
2018-09-09 15:57:49 +0200

    Fix merge issue

--- src/app/autocomplete-single-search/autocomplete-single-search.component.ts
+++ src/app/autocomplete-single-search/autocomplete-single-search.component.ts
@@ -1,9 +1,8 @@
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { Component, ElementRef, OnInit, ViewChild, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatAutocompleteSelectedEvent, MatChipInputEvent } from '@angular/material';
+import { switchMap, debounceTime, skipWhile } from 'rxjs/operators';
 import { Observable, empty } from 'rxjs';
-import { debounceTime, switchMap, skipWhile} from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
 
 @Component({
   selector: 'autocomplete-single-search',
@@ -11,31 +10,24 @@ import { debounceTime, switchMap, skipWhile} from 'rxjs/operators';
   styleUrls: ['./autocomplete-single-search.component.css']
 })
 export class AutocompleteSingleSearchComponent implements OnInit {
-  @Input() placeholder: string;
-  @Input() autocompletionCallback: Function;
-  @Output() chipListChanged: EventEmitter<string[]> = new EventEmitter();
-  visible = true;
-  selectable = true;
-  removable = true;
-  addOnBlur = false;
-  separatorKeysCodes: number[] = [ENTER, COMMA];
-  inputCtrl = new FormControl();
-  filteredTerms: Observable<string[]>;
-  items: string[] = [];
-
+  @Input() public placeholder: string;
+  @Input() public autocompletionCallback: Function;
+  @Output() resultChanged: EventEmitter<string> = new EventEmitter();
+  public inputCtrl = new FormControl();
   @ViewChild('inputRef') private inputElement: ElementRef<HTMLInputElement>;
 
-  constructor() {
-  }
+  private filteredTerms: Observable<string[]>;
+
+  constructor() { }
 
   ngOnInit(): void {
     this.filteredTerms = this.inputCtrl.valueChanges.pipe(
-      debounceTime(1000),
+      debounceTime(500),
       skipWhile(value => {
         return value && value.length < 2;
       }),
       switchMap(value => {
-        if(value===null) {
+        if (value === null) {
           return [];
         }
         return this.autocompletionCallback(value);
@@ -43,39 +35,14 @@ export class AutocompleteSingleSearchComponent implements OnInit {
      );
   }
 
-  add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
-
-    if ((value || '').trim()) {
-      this.items.push(value.trim());
-    }
-
-    if (input) {
-      input.value = '';
-    }
-
-    this.inputCtrl.setValue(null);
-    this.filteredTerms = empty();
-    this.chipListChanged.emit(this.items);
-  }
-
-  remove(ingredient: string): void {
-    const index = this.items.indexOf(ingredient);
-
-    if (index >= 0) {
-      this.items.splice(index, 1);
-    }
-    this.filteredTerms = empty();
-    this.chipListChanged.emit(this.items);
-  }
-
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.items.push(event.option.viewValue);
-    this.inputElement.nativeElement.value = '';
-    this.inputCtrl.setValue(null);
     this.filteredTerms = empty();
-    this.chipListChanged.emit(this.items);
+    this.resultChanged.emit(event.option.viewValue);
+  }
+
+  enterPressed() {
+    this.filteredTerms = empty();
+    this.resultChanged.emit(this.inputElement.nativeElement.value);
   }
 
 }
