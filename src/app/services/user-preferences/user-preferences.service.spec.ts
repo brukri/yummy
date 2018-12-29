cbc8770ef070c41e17e39c2ef7defb3fe9d45db8
Dario <dariovonaesch@gmail.com>
2018-12-29 18:08:32 +0100

    Extended tests for user-preferences.service

--- src/app/services/user-preferences/user-preferences.service.spec.ts
+++ src/app/services/user-preferences/user-preferences.service.spec.ts
@@ -17,14 +17,34 @@ describe('UserPreferencesService', () => {
   }));
 
   it('should store and remove favorites', inject([UserPreferencesService], (service: UserPreferencesService) => {
-    expect(service).toBeTruthy();
-
     service.initWithAuth(authManagement);
     expect(service.getFavorites().length).toEqual(0);
     service.addToFavorites('123');
     expect(service.getFavorites().length).toEqual(1);
+    expect(service.getFavorites()[0]).toEqual('123');
+    expect(service.isFavorite('123')).toEqual(true);
     service.removeFromFavorites('123');
     expect(service.getFavorites.length).toEqual(0);
+  }));
+  
+  it('should store and remove diets', inject([UserPreferencesService], (service: UserPreferencesService) => {
+    service.initWithAuth(authManagement);
+    expect(service.getDiets().length).toEqual(0);
+    service.saveDiets(['vegan']);
+    expect(service.getDiets().length).toEqual(1);
+    expect(service.getDiets()[0]).toEqual('vegan');
+    service.saveDiets([]);
+    expect(service.getDiets().length).toEqual(0);
+  }));
+
+  it('should store and remove intolerances', inject([UserPreferencesService], (service: UserPreferencesService) => {
+    service.initWithAuth(authManagement);
+    expect(service.getIntolerances().length).toEqual(0);
+    service.saveIntolerances(['lacto']);
+    expect(service.getIntolerances().length).toEqual(1);
+    expect(service.getIntolerances()[0]).toEqual('lacto');
+    service.saveIntolerances([]);
+    expect(service.getIntolerances().length).toEqual(0);
   }));
 
 });
